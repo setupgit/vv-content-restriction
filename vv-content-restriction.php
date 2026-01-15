639c0d7a08614a90728251808809657759caa16d
setupgit <132734627+setupgit@users.noreply.github.com>
2026-01-15 18:50:48 +0530

    Create vv-content-restriction.php
    create main file now

---
+++ vv-content-restriction.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: VV Content Restriction
+Description: Restrict content using shortcodes and provide membership system.
+Version: 1.0.0
+Author: Vishal Verma
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Plugin bootstrap (real logic later)
